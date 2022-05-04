7fbb94334bc3efd6d29704efe0447286d198b893
fevi <foevaroma@gmail.com>
2022-05-04 15:03:47 +0200

    fixed bugs, completed filter functionality

--- routes/pet.routes.js
+++ routes/pet.routes.js
@@ -13,60 +13,79 @@ const { populate } = require("../models/Pet.model");
 // const pet = req.params.pet
 // url -> /pets/cat
 router.get("/pets", (req, res, next) => {
-    const {animal, location, gender} = req.query;
-  let searchedPet;
-   if (location !== undefined){
-   
+    const { animal, location, gender } = req.query;
+    console.log("req.query", req.query)
+    let searchedPet;
+    if (animal == undefined) {
+
+        searchedPet = {}
+    }
+    else if (gender == "" && location == "") {
+        searchedPet = {
+            animal: animal
+        }
+    }
+
+    else if (location !== "" && gender !== "") {
         searchedPet = {
             animal: animal,
-            location: location,
-            gender: gender
-     }
+            gender: gender,
+            location: location
+        }
     }
-    else {
-      searchedPet = {}
-  }
-   
-  
-   console.log(searchedPet);
+    else if (gender !== "") {
+        searchedPet = {
+            animal: animal,
+            gender: gender
+        }
+    }
+    else if (location !== "") {
+        searchedPet = {
+            animal: animal,
+            location: location
+        }
+    }
 
-    Pet.find(searchedPet)
-.populate("user")
 
+
+    console.log("searchedPet", searchedPet);
+
+Pet.find(searchedPet)
     .then((petsArr) => {
-        const animals = Pet.schema.path("animal").enumValues; 
-            res.render("pets/pets-list", { pets: petsArr, animals})
-        })
-        .catch(err => {
-            console.log("error getting pets from DB", err)
-            next(err);
-        });
+        console.log("foundPet", petsArr);
+        const animals = Pet.schema.path("animal").enumValues;
+        res.render("pets/pets-list", { pets: petsArr, animals })
+    })
+    .catch(err => {
+        console.log("error getting pets from DB", err)
+        next(err);
+    });
 
 
 });
 
 //FILTER PETS
 
-router.post('/pets', (req, res) => {
-  //  const {location, gender, animal} = req.body;
+// router.post('/pets', (req, res) => {
+//   //  const {location, gender, animal} = req.body;
 
-    let filter ={}; 
-    if(req.body.animal){  
-    filter.animal = req.body.animal;
- }
-//  if(req.body.location){  
-//     filter.location = req.body.location;
+//     let filter ={}; 
+//     if(req.body.animal){  
+//     filter.animal = req.body.animal;
 //  }
- if(req.body.gender){  
-    filter.gender = req.body.gender;
- }
- console.log(filter)
-    Pet.find(filter)
-    .then ((pets) => {
-    console.log(pets)
- //     res.render('pets-list', { gender, animal, pets: result});
-    })
-  })
+// //  if(req.body.location){  
+// //     filter.location = req.body.location;
+// //  }
+//  if(req.body.gender){  
+//     filter.gender = req.body.gender;
+//  }
+//  console.log(filter)
+//     Pet.find(filter)
+//     .then ((pets) => {
+//     console.log(pets)
+//  //     res.render('pets-list', { gender, animal, pets: result});
+//     })
+//   })
 
 
 
@@ -74,19 +93,19 @@ router.post('/pets', (req, res) => {
 
 router.get('/mypets', (req, res, next) => {
 
-   const { _id } = req.session.currentUser;
-     Pet.find({user: _id})
-     .populate("user")
-      .then((myPets) => res.render('pets/my-pets', { pets: myPets }))
-      .catch((err) => next(err));
-  });
+    const { _id } = req.session.currentUser;
+    Pet.find({ user: _id })
+        .populate("user")
+        .then((myPets) => res.render('pets/my-pets', { pets: myPets }))
+        .catch((err) => next(err));
+});
 
- 
+
 
 router.get("/pets/create", (req, res, next) => {
-   const animals = Pet.schema.path("animal").enumValues;
-  
-            res.render("pets/pet-create", { animals });
+    const animals = Pet.schema.path("animal").enumValues;
+
+    res.render("pets/pet-create", { animals });
 
 })
 
@@ -97,7 +116,7 @@ router.get("/pets/create", (req, res, next) => {
 
 router.post('/pets/create', (req, res, next) => {
 
-  
+
     const newPet = {
         name: req.body.name,
         description: req.body.description,
@@ -108,11 +127,11 @@ router.post('/pets/create', (req, res, next) => {
         healthIssues: req.body.healthIssues,
         location: req.body.location,
         user: req.session.currentUser._id,
-      //  location: req.session.currentUser.location
+        //  location: req.session.currentUser.location
     }
-   
+
     Pet.create(newPet)
-   
+
         .then((petFromDB) => {
             res.redirect("/pets")
         })
@@ -147,7 +166,7 @@ router.get("/pets/:petId", (req, res, next) => {
 router.get("/pets/:petId/edit", (req, res, next) => {
     const id = req.params.petId;
     Pet.findById(id)
-    .populate("user")
+        .populate("user")
         .then((petDetails) => {
             res.render("pets/pet-edit", petDetails);
         })
@@ -165,7 +184,7 @@ router.post("/pets/:petId/edit", isLoggedIn, (req, res, next) => {
     const newDetails = {
         name: req.body.name,
         description: req.body.description,
-     
+
         dateOfBirth: req.body.dateOfBirth,
         image: req.body.image,
         healthIssues: req.body.healthIssues,
